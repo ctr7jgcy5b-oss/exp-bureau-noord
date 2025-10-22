ddb97d79ffa1a99d08ba0c78f96da6681890e128
arjan <arjanpetersen@hotmail.com>
2025-10-23 00:40:36 +0200

    xxcv

--- js/app.js
+++ js/app.js
@@ -18,12 +18,4 @@ if (process.env.NODE_ENV !== 'test') {
 
 module.exports = app;
 
-//const request = require('supertest');
-// const app = require('./app');
-
-describe('App Tests', () => {
-    test('GET / should return 200', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
-});
+;
